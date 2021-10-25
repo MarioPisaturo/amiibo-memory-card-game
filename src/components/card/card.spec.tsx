54974d6697faa14a8b8b956c544094506b05917b
Mario Pisaturo <mario.pisaturo@skytv.it>
2021-10-25 11:03:53 +0200

    feat: update card component and tests

--- src/components/card/card.spec.tsx
+++ src/components/card/card.spec.tsx
@@ -1,7 +1,24 @@
 import * as React from 'react';
 import { shallow } from 'enzyme';
 
-import Card, { ICard } from './card';
+import Card, {
+  ICard,
+  CARD_ITEM_STATE_HIDDEN,
+  CARD_ITEM_STATE_BACKFACED,
+  CARD_ITEM_STATE_ENABLED,
+  CARD_ITEM_STATE_MATCHED,
+} from './card';
+
+/** Card Component TEST SUITE
+ * 1. Should render a Card Component
+ * 2. Should call onCardClicked callback with correct card ID
+ * 3. should render a CARD_ITEM_STATE_BACKFACED card
+ * 4. should render a CARD_ITEM_STATE_ENABLED card
+ * 5. should render a CARD_ITEM_STATE_HIDDEN card
+ * 6. should render a CARD_ITEM_STATE_MATCHED card
+ * 7. should render a custom backface of the Card
+ * 8. ...
+ */
 
 describe('Card components', () => {
   function makeProps(
@@ -22,63 +39,11 @@ describe('Card components', () => {
     const wrapper = shallow(<Card {...props} />);
     expect(wrapper).toHaveLength(1);
 
+    expect(wrapper.find('div').at(0).prop('data-state')).toEqual(CARD_ITEM_STATE_BACKFACED);
     expect(wrapper.find('img').at(0).prop('src')).toEqual('./assets/poketball.png'); // DEFAULT PROP BACKFACE
     expect(wrapper.find('img').at(1).prop('src')).toEqual(props.imageUrl);
-    expect(wrapper.find('div').at(2).prop('className')).toMatch(/card-face card-back-face/); // BACK FACE showed
-  });
-
-  it('should render a BACKFACED Card ', () => {
-    const props: ICard = makeProps();
-    const wrapper = shallow(<Card {...props} />);
-
-    expect(wrapper).toHaveLength(1);
-    expect(wrapper.find('div').at(0).prop('data-state')).toEqual('backfaced');
-    expect(wrapper.find('div').at(0).prop('className')).toEqual('card');
-  });
-
-  it('should render a ENABLED Card ', () => {
-    const props: ICard = {
-      ...makeProps(),
-      state: 'enabled',
-    };
-    const wrapper = shallow(<Card {...props} />);
-
-    expect(wrapper).toHaveLength(1);
-    expect(wrapper.find('div').at(0).prop('data-state')).toEqual('enabled');
-    expect(wrapper.find('div').at(0).prop('className')).toEqual('card is-flipped');
-  });
-
-  it('should render a HIDDEN Card ', () => {
-    const props: ICard = {
-      ...makeProps(),
-      state: 'hidden',
-    };
-    const wrapper = shallow(<Card {...props} />);
-
-    expect(wrapper).toHaveLength(1);
-    expect(wrapper.find('div').at(0).prop('data-state')).toEqual('hidden');
-    expect(wrapper.find('div').at(0).prop('className')).toEqual('card is-inactive');
-  });
-
-  it('should render a MATCHED Card - isFlipped', () => {
-    const props: ICard = {
-      ...makeProps(),
-      state: 'matched',
-    };
-    const wrapper = shallow(<Card {...props} />);
-
-    expect(wrapper).toHaveLength(1);
-    expect(wrapper.find('div').at(0).prop('data-state')).toEqual('matched');
-    expect(wrapper.find('div').at(0).prop('className')).toEqual('card is-flipped');
-  });
-
-  it('should render a custom backface Card ', () => {
-    const props: ICard = {
-      ...makeProps(),
-      backfaceUrl: 'my/custom/backface/img',
-    };
-    const wrapper = shallow(<Card {...props} />);
-    expect(wrapper.find('img').at(0).prop('src')).toEqual(props.backfaceUrl);
+    expect(wrapper.find('div').at(1).prop('className')).toMatch(/card--card-face/); // card face
+    expect(wrapper.find('div').at(2).prop('className')).toMatch(/card--card-face card--card-back-face/); // card back face
   });
 
   it('should call onCardClicked callback with correct id', () => {
@@ -92,5 +57,56 @@ describe('Card components', () => {
     rendered.find('div').at(0).simulate('click');
     expect(spyOnClick).toHaveBeenCalledTimes(1);
     expect(spyOnClick).toHaveLastReturnedWith(true); // id === 1234
+  });
+
+  it('should render a BACKFACED Card ', () => {
+    const props: ICard = makeProps();
+    const wrapper = shallow(<Card {...props} />);
+
+    expect(wrapper).toHaveLength(1);
+    expect(wrapper.find('div').at(0).prop('data-state')).toEqual(CARD_ITEM_STATE_BACKFACED);
+    expect(wrapper.find('div').at(0).prop('className')).toEqual('card');
+  });
+
+  it('should render a ENABLED Card ', () => {
+    const props: ICard = {
+      ...makeProps(),
+      state: 'enabled',
+    };
+    const wrapper = shallow(<Card {...props} />);
+
+    expect(wrapper).toHaveLength(1);
+    expect(wrapper.find('div').at(0).prop('data-state')).toEqual(CARD_ITEM_STATE_ENABLED);
+  });
+
+  it('should render a HIDDEN Card ', () => {
+    const props: ICard = {
+      ...makeProps(),
+      state: 'hidden',
+    };
+    const wrapper = shallow(<Card {...props} />);
+
+    expect(wrapper).toHaveLength(1);
+    expect(wrapper.find('div').at(0).prop('data-state')).toEqual(CARD_ITEM_STATE_HIDDEN);
+  });
+
+  it('should render a MATCHED Card - isFlipped', () => {
+    const props: ICard = {
+      ...makeProps(),
+      state: 'matched',
+    };
+    const wrapper = shallow(<Card {...props} />);
+
+    expect(wrapper).toHaveLength(1);
+    expect(wrapper.find('div').at(0).prop('data-state')).toEqual(CARD_ITEM_STATE_MATCHED);
+  });
+
+  it('should render a custom backface of the Card', () => {
+    const props: ICard = {
+      ...makeProps(),
+      backfaceUrl: 'my/custom/backface/img',
+    };
+    const wrapper = shallow(<Card {...props} />);
+    expect(wrapper.find('img').at(0).prop('src')).toEqual(props.backfaceUrl);
   });
 });
